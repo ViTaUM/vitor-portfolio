4c2548453d3d8c9a4ade78f4500c8ef57184d3d2
ViTaUM <vitaum.alves@gmail.com>
2024-04-16 12:47:36 -0300

    atualização do bugs

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,19 +1,21 @@
-import { BrowserRouter } from "react-router-dom"
-import { Container } from "./styles"
-import ScrollAnimation from "react-animate-on-scroll"
-import Illustration from "../../assets/vitor-home.png"
-import { NavHashLink } from "react-router-hash-link"
-import linkedin from '../../assets/linkedin.svg'
-import githubIcon from '../../assets/github.svg'
-import whatsapp from '../../assets/whatsapp.svg'
-import Hello from '../../assets/Hello.gif'
-import telegram from '../../assets/telegram.svg'
+import { BrowserRouter } from "react-router-dom";
+import { Container } from "./styles";
+import ScrollAnimation from "react-animate-on-scroll";
+import Illustration from "../../assets/vitor-home.jpg";
+import { NavHashLink } from "react-router-hash-link";
+import linkedin from "../../assets/linkedin.svg";
+import githubIcon from "../../assets/github.svg";
+import whatsapp from "../../assets/whatsapp.svg";
+import Hello from "../../assets/Hello.gif";
+import telegram from "../../assets/telegram.svg";
 export function Hero() {
   return (
     <Container id="home">
       <div className="hero-text">
         <ScrollAnimation animateIn="fadeInUp">
-          <p>Olá <img src={Hello} alt="Hello" width="20px"/>, meu nome é</p>
+          <p>
+            Olá <img src={Hello} alt="Hello" width="20px" />, meu nome é
+          </p>
         </ScrollAnimation>
         <ScrollAnimation animateIn="fadeInUp" delay={0.2 * 1000}>
           <h1>Vitor Alves</h1>
@@ -26,38 +28,42 @@ export function Hero() {
         </ScrollAnimation>
         <ScrollAnimation animateIn="fadeInUp" delay={0.8 * 1000}>
           <BrowserRouter>
-            <NavHashLink smooth to="#contact" className="button">Contato</NavHashLink>
+            <NavHashLink smooth to="#contact" className="button">
+              Contato
+            </NavHashLink>
           </BrowserRouter>
         </ScrollAnimation>
         <ScrollAnimation animateIn="fadeInUp" delay={1 * 1000}>
-      <div className="social-media"><a
-        href="https://www.linkedin.com/in/vitor-silva-alves/"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img src={linkedin} alt="Linkedin" />
-      </a>
-        <a
-          href="https://github.com/ViTaUM"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={githubIcon} alt="GitHub" />
-        </a>
-        <a
-          href="https://api.whatsapp.com/send/?phone=5571993555912&text=Oi,%20Vitor!%20Vim%20do%20teu%20portif%C3%B3lio,%20quero%20falar%20contigo!"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={whatsapp} alt="Whatsapp" />
-        </a>
-        <a
-          href="https://t.me/5571993555912"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={telegram} alt="telegram" />
-        </a></div>
+          <div className="social-media">
+            <a
+              href="https://www.linkedin.com/in/vitor-silva-alves/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={linkedin} alt="Linkedin" />
+            </a>
+            <a
+              href="https://github.com/ViTaUM"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={githubIcon} alt="GitHub" />
+            </a>
+            <a
+              href="https://api.whatsapp.com/send/?phone=5571993555912&text=Oi,%20Vitor!%20Vim%20do%20teu%20portif%C3%B3lio,%20quero%20falar%20contigo!"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={whatsapp} alt="Whatsapp" />
+            </a>
+            <a
+              href="https://t.me/5571993555912"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={telegram} alt="telegram" />
+            </a>
+          </div>
         </ScrollAnimation>
       </div>
       <div className="hero-image">
@@ -66,5 +72,5 @@ export function Hero() {
         </ScrollAnimation>
       </div>
     </Container>
-  )
+  );
 }
